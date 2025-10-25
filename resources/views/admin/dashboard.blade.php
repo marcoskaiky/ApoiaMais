f309a7a96156e224ed8f5247c1f1acf7870134ae
Kaiky <kaikymassaki2005@gmail.com>
2025-10-25 16:13:25 -0300

    fix css

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Dashboard Admin - Apoia+</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/backend.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/admin.css'])
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
